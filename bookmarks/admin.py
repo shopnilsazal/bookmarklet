ba86f29690c375ddb6390ee035c113bb1e054078
Rafiqul Hasan <rafiqul@themexpert.com>
2016-10-30 16:33:16 +0600

    Category and Page Models created with Admin.

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from bookmarks.models import Category, Page
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name', 'views', 'likes']
+    list_display = ('name', 'views', 'likes')
+
+
+class PageAdmin(admin.ModelAdmin):
+    fields = ['title', 'category', 'url', 'views']
+    list_display = ('title', 'category', 'url', 'views')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
